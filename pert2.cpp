289eebd66380c6eaf4b6029e05c3aea8f6ce2a5b
paw2kk <wndxakk@gmail.com>
2025-03-04 11:45:51 +0700

    introduction C++

---
+++ pert2.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    char jeniskelamin = 'L';
+    string jenis = "Laki-Laki";
+    float x = 10.5;
+    int y = 5;
+    bool check = true;
+
+    cout << "Selamat Datang di TI UMY\n";
+    cout << "Yogyakarta" << endl;
+    cout << "Indonesia";
+}
